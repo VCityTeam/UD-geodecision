e407079eb6885c58e2408ced8a9b2a59b3f3a146
thomas <thomleysens@gmail.com>
2020-02-21 19:47:37 +0100

    WIP on dashboard docker

--- docker/Dashboard_DockerContext/dashboard/main.py
+++ docker/Dashboard_DockerContext/dashboard/main.py
@@ -33,6 +33,8 @@ from constants import set_para
 #Get json_config from arg
 json_config = sys.argv[1]
 
+print ("JSON", json_config)
+
 #Load params JSON file
 with open(json_config) as f: 
     params = json.load(f)
--- docker/Dashboard_DockerContext/entrypoint.py
+++ docker/Dashboard_DockerContext/entrypoint.py
@@ -3,6 +3,7 @@ import json
 import os
 import shutil
 
+
 subprocess.call(
         [
                 "conda", 
@@ -11,13 +12,16 @@ subprocess.call(
                 "geodecision",
                 "bokeh",
                 "serve",
+#                "--show",
+#                "--allow-websocket-origin=localhost:5006",
                 "dashboard",
                 "--args",
                 "/Input/bokeh_config.json"
                 ]
         )
-
-json_config=os.path.join('/Input', 'bokeh_config.json')
-with open(json_config) as f: 
-   params = json.load(f)
+#print ("YOUPI")
+#json_config=os.path.join('/Input', 'bokeh_config.json')
+#with open(json_config) as f: 
+#   params = json.load(f)
+#   print ("PARAMS", params)
 
